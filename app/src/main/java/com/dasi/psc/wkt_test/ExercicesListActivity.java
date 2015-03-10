8ee56c769e203d6ff7de42c82cfc9b070d1f89ac
CHARIOT Jordan <jordan.chariot@gmail.com>
2015-03-10 16:10:51 +0100

    Ajout d'élèment dans la liste des exercices.

--- app/src/main/java/com/dasi/psc/wkt_test/ExercicesListActivity.java
+++ app/src/main/java/com/dasi/psc/wkt_test/ExercicesListActivity.java
@@ -1,9 +1,13 @@
 package com.dasi.psc.wkt_test;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class ExercicesListActivity extends ActionBarActivity {
@@ -12,15 +16,14 @@ public class ExercicesListActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercices_list);
+
+        String[] exercises = {"Abdominal crunch", "Push-up", "Squat", "Plank"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, exercises);
+        //getListView().setAdapter(adapter);
+        ListView myList = (ListView) findViewById(android.R.id.list);
+        myList.setAdapter(adapter);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_exercices_list, menu);
-        return true;
-    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -35,5 +38,12 @@ public class ExercicesListActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /** Called when the user clicks the + button */
+    public void openAddWorkoutScreen(View view) {
+        Intent intent = new Intent(this, addAWorkoutActivity.class);
+
+        startActivity(intent);
     }
 }
